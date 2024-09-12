069e0089c4c627d8e48422b4ca4ea8799b2d80d2
EmmettOsborne <EmmettOsborne.01@gmail.com>
2024-09-11 20:20:26 -0600

    Complete syncronizer.

--- hdl/synchronizer/synchronizer.vhd
+++ hdl/synchronizer/synchronizer.vhd
@@ -9,3 +9,17 @@ entity synchronizer is
     );
 end entity synchronizer;
 
+architecture behavioral of synchronizer is
+    signal stg1, stg2 : std_ulogic;
+begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            stg1 <= async;    --First flipflop stage
+            stg2 <= stg1;   --Second flipflop stage
+        end if;
+    end process;
+
+    sync <= stg2;   --Output sync'd signal
+
+end architecture behavioral;
